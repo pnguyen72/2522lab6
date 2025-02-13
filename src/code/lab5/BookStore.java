959382484cb17461e2bd5b8799815857e743564d
A. Santos <angelo.rws@gmail.com>
2025-02-13 08:24:24 -0800

    Implement main (currently missing methods)

--- src/code/lab5/BookStore.java
+++ src/code/lab5/BookStore.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import lab4.*;
+
 /**
  * Contains a list of many novels and
  * methods to manipulate them,
@@ -89,12 +91,39 @@ public class BookStore
         final BookStore   bookstore;
         final Novel       oldest;
         final List<Novel> fifteenCharTitles;
-        bookstore = new BookStore("Classic Novels Collection");
+
+        BookStore bookstore = new BookStore("Classic Novels Collection");
+
         System.out.println("All Titles in UPPERCASE:");
         bookstore.printAllTitles();
+
         System.out.println("\nBook Titles Containing 'the':");
         bookstore.printBookTitle("the");
+
         System.out.println("\nAll Titles in Alphabetical Order:");
         bookstore.printTitlesInAlphaOrder();
+
+        System.out.println("\nBooks from the 2000s:");
+        bookstore.printGroupByDecade(2000);
+
+        System.out.println("\nLongest Book Title:");
+        bookstore.getLongest();
+
+        System.out.println("\nIs there a book written in 1950?");
+        System.out.println(bookstore.isThereABookWrittenBetween(1950));
+
+        System.out.println("\nHow many books contain 'heart'?");
+        System.out.println(bookstore.howManyBooksContain("heart"));
+
+        System.out.println("\nPercentage of books written between 1940 and 1950:");
+        System.out.println(bookstore.whichPercentWrittenBetween(1940, 1950) + "%");
+
+        System.out.println("\nOldest book:");
+        oldest = bookstore.getOldestBook();
+        System.out.println(oldest.getTitle() + " by " + oldest.getAuthorName() + ", " + oldest.getYearPublished());
+
+        System.out.println("\nBooks with titles 15 characters long:");
+        fifteenCharTitles = bookstore.getBooksThisLength(15);
+        fifteenCharTitles.forEach(novel -> System.out.println(novel.getTitle()));
     }
 };
