23e2a0975c62f6fc2839e9ebf20e1e74e3cebfb7
Justin Cardas <justincardas@gmail.com>
2025-02-19 21:46:25 -0800

    Anonymous Inner Class Overriding compare()

--- src/code/lab6/BookStore.java
+++ src/code/lab6/BookStore.java
@@ -90,6 +90,17 @@ public class BookStore<T extends Literature>
         bookTitles.forEach(System.out::println);
     }
 
+    public void addNovelsToCollection(final List<? super Novel> novelCollection)
+    {
+        for(final T item : items)
+        {
+            if(item instanceof Novel)
+            {
+                novelCollection.add((Novel) item); // casts type Novel
+            }
+        }
+    }
+
     public static void main(final String[] args)
     {
         final BookStore<Literature> store;
@@ -97,6 +108,7 @@ public class BookStore<T extends Literature>
 
         store.addItem(new Novel("War and Peace"));
         store.addItem(new ComicBook("Spider-Man"));
+        store.addItem(new Novel("Novel With a Really Long Name That is Very Long"));
         store.addItem(new Magazine("National Geographic"));
 
         store.printItems(); // Should print titles from different item types
@@ -113,5 +125,32 @@ public class BookStore<T extends Literature>
         System.out.println(System.lineSeparator());
 
         store.printTitlesInAlphaOrder();
+
+
+        List<Novel> novels = new ArrayList<>();
+
+        store.addNovelsToCollection(novels);
+
+        System.out.println(System.lineSeparator());
+        System.out.println("Novels in Novels Array:");
+        for(Novel novel : novels)
+        {
+            System.out.println(novel.getTitle());
+        }
+
+        System.out.println(System.lineSeparator());
+
+        // Anonymous Inner Class
+        store.items.sort(new Comparator<Literature>()
+        {
+            @Override
+            public int compare(final Literature o1, final Literature o2)
+            {
+                return Integer.compare(o1.getTitle().length(),
+                                       o2.getTitle().length());
+            }
+        });
+
+        store.printItems();
     }
 }
