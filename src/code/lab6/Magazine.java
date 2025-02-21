9c1896a7a7aa26b5c69f111926f038dee9397215
Armaan <abrar158@my.bcit.ca>
2025-02-21 11:21:10 -0800

    Commented Magazine class

--- src/code/lab6/Magazine.java
+++ src/code/lab6/Magazine.java
@@ -1,14 +1,32 @@
 package lab6;
 
+/**
+ * Represents a magazine with a title that extends
+ * a piece of literature.
+ *
+ * @author Justin
+ * @author Felix
+ * @author Armaan
+ */
 public class Magazine extends Literature
 {
     private final String title;
 
+    /**
+     * Constructs a magazine with the specified title
+     * @param title title of magazine
+     */
     Magazine(final String title)
     {
         this.title = title;
     }
 
+    /**
+     * Gets the title of the magazine.
+     * Overrides abstract method from
+     * parent class.
+     * @return the title of the magazine.
+     */
     @Override
     public String getTitle()
     {
